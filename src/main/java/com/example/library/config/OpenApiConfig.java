ac38c6ad77c3d3d86d6fc92d2848f3efbb01b1f5
Nandha gopal <118921488+Nandhagopal23@users.noreply.github.com>
2026-03-24 12:15:08 +0530

    Add Swagger OpenAPI with JWT bearer support (#7)

---
+++ src/main/java/com/example/library/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI libraryOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Management API")
+                        .version("v1")
+                        .description("API documentation for Library Management system"))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
